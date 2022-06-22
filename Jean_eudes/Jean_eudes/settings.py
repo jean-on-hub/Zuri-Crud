e118ab7b9eff202f875f273aa7a62a263ee8d616
jean-on-hub <jean.eudes@amalitech.org>
2022-06-22 17:14:43 +0000

    secret key

--- Jean_eudes/Jean_eudes/settings.py
+++ Jean_eudes/Jean_eudes/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f*kn7m=zek%@sv6$ed85n=niovhoc($_%lx__9tle5q9uga%+f'
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
