2340c021f94e8db44bc4695abe2b3fc2d54bf7dc
jean-on-hub <jean.eudes@amalitech.org>
2022-06-23 08:57:55 +0000

    key

--- Jean_eudes/blog/views.py
+++ Jean_eudes/blog/views.py
@@ -1,7 +1,7 @@
 from msilib.schema import Class, ListView
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView,DetailView
+from django.views.generic import  TemplateView, CreateView,DetailView,UpdateView
 # Create your views here.
 class PostListView(ListView):
     model = Post 
@@ -11,3 +11,11 @@ class PostCreateView(CreateView):
     success_url = reverse_lazy('blog:all')
 class PostDetailView(DetailView):
     model = Post
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+class  PostDeleteView(UpdateView):
+    odel = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
